7f5141e08634b2d613dcdc580f0c9ab1b8cdb41e
Brandon Allison <brandona8128@outlook.com>
2019-05-03 11:37:10 -0400

    server up/migrations created but not completed

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const server = express();
+const helmet = require('helmet');
+const cors = require('cors');
+
+server.use(cors());
+server.use(helmet());
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send(`
+    ----Server is Running----
+    `)
+});
+
+module.exports = server;
